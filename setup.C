3106fe4fba3a8df3824d64ba5f240afb5b106f31
bmahakud <bmahakud@users.noreply.github.com>
2014-04-12 04:36:44 +0530

    Update setup.C

--- setup.C
+++ setup.C
@@ -1,4 +1,3 @@
-
 #include<iostream>
 #include<TROOT.h>
 using namespace std;
@@ -11,10 +10,10 @@ void setup(){
    
  
    gSystem->AddIncludePath("-I$CMSSW_BASE/src/BaconAna/DataFormats/interface/");
-   gSystem->AddIncludePath("-I/afs/cern.ch/cms/slc5_amd64_gcc462/external/fastjet/3.0.1-cms2/include");
+   gSystem->AddIncludePath("-I/afs/cern.ch/cms/slc5_amd64_gcc462/external/fastjet/3.0.1-cms3/include");
    gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libBaconAnaDataFormats.so");
-   gSystem->Load("/afs/cern.ch/cms/slc5_amd64_gcc462/external/fastjet/3.0.1-cms2/lib/libfastjet.so");
-   gSystem->Load("/afs/cern.ch/cms/slc5_amd64_gcc462/external/fastjet/3.0.1-cms2/lib/libfastjettools.so");   
+   gSystem->Load("/afs/cern.ch/cms/slc5_amd64_gcc462/external/fastjet/3.0.1-cms3/lib/libfastjet.so");
+   gSystem->Load("/afs/cern.ch/cms/slc5_amd64_gcc462/external/fastjet/3.0.1-cms3/lib/libfastjettools.so");   
    gSystem->Load("libFWCoreFWLite.so");
    AutoLibraryLoader::enable(); 
    gSystem->Load("libDataFormatsFWLite.so");
