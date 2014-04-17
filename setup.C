c505a36ac00fc4a23184ed7e88bb29399d87b4ac
bmahakud <bmahakud@users.noreply.github.com>
2014-04-17 12:06:13 +0530

    Update setup.C

--- setup.C
+++ setup.C
@@ -22,53 +22,7 @@ void setup(){
 
 
 
-   gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libBaconProdNtupler.so");
-   gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libBaconProdUtils.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libCMGToolsExternal.so");   
-
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libCommonToolsRecoUtils.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libCondFormatsEgammaObjects.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libCondFormatsJetMETObjects.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libDataFormatsJetReco.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libDataFormatsMETReco.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libEGammaEGammaAnalysisTools.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libJetMETCorrectionsType1MET.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libJetSubstructureSubstructureTools.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libJetToolsAnalyzerToolbox.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libMMozerpowhegweight.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libMuScleFitCalibration.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libPhysicsToolsKinFitter.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libPhysicsToolsPatAlgos.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libPhysicsToolsPatUtils.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libRecoJetsJetProducers.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libRecoMETMETPUSubtraction.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/libSHarperHEEPAnalyzer.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginBaconProdNtuplerPlugins.so");
-
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginCMGToolsExternalCapabilities.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginCMGToolsExternalsplugins.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginCondFormatsEgammaObjectsCapabilities.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginCondFormatsJetMETObjectsCapabilities.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginDataFormatsJetRecoCapabilities.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginDataFormatsMETRecoCapabilities.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginJetMETCorrectionsType1MET_plugins.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginJetToolbox.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginJetToolsAnalyzerToolboxCapabilities.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginMETFilters_plugins.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginMMozerpowhegweightPlugins.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginMuScleFitCalibrationPlugins.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginPFPUAssoMapPlugins.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginPhysicsToolsPatAlgos_plugins.so");
-
-
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginPhysicsToolsPatAlgos_testAnalyzers.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginPhysicsToolsPatUtilsCapabilities.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginPhysicsToolsPatUtils_plugins.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginRecoJetsJetProducers_plugins.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginRecoMETMETAnalyzers.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginRecoMETMETPUSubtraction_plugins.so");
- gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginRecoMETMETProducers.so");
-gSystem->Load("$CMSSW_BASE/lib/$SCRAM_ARCH/pluginSHarperHEEPAnalyzer_plugins.so");
+  
 
 
 
